731e603b69845cfe68ea7329900a70c8bcdd879b
Ishibashi Tomoharu <t.ishibashi@raisex.jp>
2023-07-29 16:10:15 +0900

    Initial commit

---
+++ test/aws-cdk-study-js.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const AwsCdkStudyJs = require('../lib/aws-cdk-study-js-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-study-js-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new AwsCdkStudyJs.AwsCdkStudyJsStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
