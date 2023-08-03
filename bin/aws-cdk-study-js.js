a4b79954fa142b8a40c24fe1e93e9f387a65030f
Ishibashi Tomoharu <t.ishibashi@raisex.jp>
2023-08-03 19:07:28 +0900

    タグ付け

--- bin/aws-cdk-study-js.js
+++ bin/aws-cdk-study-js.js
@@ -5,4 +5,9 @@ const { AwsCdkStudyJsStack } = require('../lib/aws-cdk-study-js-stack');
 const { ApplicationStack } = require('../lib/application-stack');
 
 const app = new cdk.App();
-new ApplicationStack(app, 'ApplicationStack', {})
+const applicationStack = new ApplicationStack(app, 'ApplicationStack', {})
+
+// 一括でタグ付け
+cdk.Tags.of(app).add('workshop', 'aws-cdk-study')
+cdk.Tags.of(applicationStack).add('workshop', 'aws-cdk-study')
+cdk.Tags.of(applicationStack).add('env', 'sandbox')
