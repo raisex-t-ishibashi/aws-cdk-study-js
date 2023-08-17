17a55071fb775a4a53eb6c5fc3e213ef2e075454
Ishibashi Tomoharu <t.ishibashi@raisex.jp>
2023-08-17 10:09:15 +0900

    IAMロールのコンストラクト化

--- construct/iam.js
+++ construct/iam.js
@@ -1,23 +1,61 @@
-const {ServicePrincipal, Role, ManagedPolicy} = require("aws-cdk-lib/aws-iam");
+const {ServicePrincipal, Role, PolicyStatement} = require("aws-cdk-lib/aws-iam");
 const {Construct} = require('constructs')
+const iam = require("aws-cdk-lib/aws-iam");
+
+class LambdaToDynamoDBAccessRole extends Construct {
+    role = null
+
+    constructor(scope, id) {
+        super(scope, id);
+
+        const role = new iam.Role(this, 'LambdaToDynamoDBAccessRole', {
+            roleName: 'LambdaToDynamoDBAccessRole',
+            assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
+            managedPolicies: [
+                iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaBasicExecutionRole'),
+                iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonDynamoDBFullAccess')
+            ]
+        })
+        this.role = role
+    }
+}
 
 class LambdaPublisherRole extends Construct {
     role = null
 
     constructor(scope, id) {
         super(scope, id);
-        this.role = new Role(this, 'LambdaToDynamoDBAccessRole2', {
-            roleName: 'LambdaToDynamoDBAccessRole2',
-            assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
-            managedPolicies: [
-                // TODO ポリシー変更
-                // ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaBasicExecutionRole'),
-                // ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaSQSQueueExecutionRole')
+
+        const policy = {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Sid": "VisualEditor0",
+                    "Effect": "Allow",
+                    "Action": [
+                        "events:PutEvents",
+                        "sns:Publish",
+                        "sqs:SendMessage"
+                    ],
+                    "Resource": [
+                        "arn:aws:sqs:ap-northeast-1:717366958267:*",
+                        "arn:aws:sns:ap-northeast-1:717366958267:*",
+                        "arn:aws:events:ap-northeast-1:717366958267:event-bus/*"
+                    ]
+                }
             ]
+        }
+
+        const role = new Role(this, 'LambdaPublisherRole', {
+            roleName: 'LambdaPublisherRole',
+            assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
         })
+        role.addToPolicy(PolicyStatement.fromJson(policy))
+        this.role = role
     }
 }
 
 module.exports = {
-    LambdaToDynamoDbAccessRole2: LambdaPublisherRole
+    LambdaToDynamoDBAccessRole,
+    // LambdaPublisherRole
 }
